2475d087392a117b1f205049490eefb749253da8
Alexrus-cyber <85308640+Alexrus-cyber@users.noreply.github.com>
2024-12-27 14:12:42 +0300

    Update function.ts

--- function.ts
+++ function.ts
@@ -1,21 +1,22 @@
 // 1. Написать функцию, которая на вход принимает целое число и возвращает факториал этого числа
-function factorial(count: number): number | string {
-  if (count < 0) {
-    return 'Значение не может быть отрицательным';
-  }
+const factorial = (count: number): number | string => {
+  if (count < 0) return 'Значение не может быть отрицательным';
+
   let result = 1;
-  for (let i = 2; i <= count; i++) {
-    result *= i;
-  }
+
+  for (let i = 2; i <= count; i++) result *= i;
+
   return result;
-}
+};
 
 //2. Написать функцию, которая на вход принимает строку и возвращает длину самого длинного слова в этой строке
 const longestLine = (str: string): string => {
   const text: string[] = str.split(' '); // разбиваем строку на слова
+
   const longestItem: string = text.reduce((longest, current) => {
     return current.length > longest.length ? current : longest; // используем метод reduce для нахождения самого длинного слова(1 значение будет самым длинным словом,
   }, ''); // 2 значение - текущее слово)
+
   return longestItem;
 };
 
@@ -23,6 +24,7 @@ const longestLine = (str: string): string => {
 // и возвращает массив, состоящий из наибольшего числа каждого представленного подмассива.
 const arrayMaxNumber = (array: number[]): number => {
   const maxNumber: number = array.reduce((a, b) => Math.max(a, b)); // используем метод reduce для нахождения самого большого числа
+
   return maxNumber;
 };
 
@@ -30,7 +32,9 @@ const arrayMaxNumber = (array: number[]): number => {
 // если она длиннее заданной максимальной длины строки(второй аргумент) и возвращает обрезанную строку с "..." в конце.
 const editLine = (str: string, count: number): string => {
   let text: string = '';
+
   str.length > count ? (text = str.substring(0, count) + '...') : (text = str); // если длина строки больше заданной длины, то обрезаем строку
+
   return text;
 };
 
@@ -38,37 +42,49 @@ const editLine = (str: string, count: number): string => {
 //каждое слово начинается с заглавной буквы, остальные в нижнем регистре
 const strUp = (str: string): string => {
   const text: string[] = str.split(' '); // разбиваем строку на слова
+
   const editorText: string[] = text.map(item => {
     return item.charAt(0).toUpperCase() + item.slice(1); // переводим первую букву в верхний регистр
   });
+
   return editorText.join(' ');
 };
 
 //6. Написать функцию, которая на вход принимает 2 массива и целое число n. Функция должна скопировать каждый элемент 1 массива
 //во 2 по порядку начиная с индекса n второго массива и вернуть полученный массив. Входные массивы не должны изменяться.
 //Например: при входных данных ([1,2,3], [4,5], 1) должно вернуться [4,1,2,3,5].
-const unionArrays = (array: number[], array2: number[], n: number): number[] | string => {
+const unionArrays = (
+  array: number[],
+  array2: number[],
+  n: number
+): number[] | string => {
   const resultArrays: number[] = []; // не изменяя исходный массив. slice делает именно это, возвращая новый массив
+
   resultArrays.push(...array, ...array2.slice(0, n)); // объединяем два массива и удаляем из второго массива n элементов
+
   return resultArrays;
 };
 
 //7. Напишите функцию, которая принимает массив в качестве входных данных и удаляет из него все "ложные значения"
-const removeFalsyValues = <T>(array: T[]): T[] => { // использую дженерик прочитал про них. Чтобы функция сама определлила свой тип
-  return array.filter(Boolean) as T[];  // по приходящим в нее пропсам, и так мы не нарушим типизацию. Чтобы случайно не написать any
-}; // объект Boolean, который отфильтровывает ложные значения.
+const removeFalsyValues = <T>(array: T[]): T[] => {
+  // использую дженерик прочитал про них. Чтобы функция сама определила свой тип
+  // по приходящим в нее пропсам, и так мы не нарушим типизацию. Чтобы случайно не написать any
+  return array.filter(Boolean) as T[]; // объект Boolean, который отфильтровывает ложные значения.
+};
 
 //8. Написать функцию, которая на вход принимает массив, состоящий из двух строк
 //Функция должна вернуть true, если строка в первом элементе массива содержит
 //все буквы строки во втором элементе массива (регистр игнорируется)
 const equalityStroke = (strArray: string[]): boolean | null | string => {
-  if (strArray.length > 2) {
+  if (strArray.length > 2)
     return 'Ошибка вы не можете создать масссив больше двух элементов';
-  }
+
   const result: boolean = strArray[0]
     .toLowerCase()
     .toUpperCase()
-    .includes(strArray[1].toLowerCase().toUpperCase()); // проверяем одинаковы ли строки приведенные к нижнему регистру и верхнему
+    .includes(strArray[1].toLowerCase().toUpperCase());
+  // проверяем одинаковы ли строки приведенные к нижнему регистру и верхнему
+
   return result.toString();
 };
 
@@ -77,10 +93,12 @@ const equalityStroke = (strArray: string[]): boolean | null | string => {
 //(второй аргумент) и вернуть их в виде двумерного массива.
 const subArrays = (array: string[], count: number): string[][] => {
   const result: string[][] = [];
+
   for (let i = 0; i < array.length; i += count) {
     const subArray = array.slice(i, i + count); // разбиваем на подмассивы длиной count
     result.push(subArray);
   }
+
   return result;
 };
 
@@ -88,10 +106,8 @@ const subArrays = (array: string[], count: number): string[][] => {
 //которая принимает массив и целое число (n) и заполняет массив числами от n до 1
 const resultRecourse: number[] = [];
 const recourse = (array: number[], count: number) => {
-  if (count <= 0) {
-    return;
-  }
+  if (count <= 0) return;
+
   array.push(count);
-  recourse(array, count - 1);
+  recourse(array, count - 1); // вызываем рекурсию
 };
-// recourse(resultRecourse, 10);
