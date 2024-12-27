b97919b9af8f68142a5be4828b37151f4bdcfb80
Alexrus-cyber <85308640+Alexrus-cyber@users.noreply.github.com>
2024-12-27 13:29:33 +0300

    Поправки

--- function.ts
+++ function.ts
@@ -1,3 +1,4 @@
+// 1. Написать функцию, которая на вход принимает целое число и возвращает факториал этого числа
 function factorial(count: number): number | string {
   if (count < 0) {
     return 'Значение не может быть отрицательным';
@@ -8,62 +9,69 @@ function factorial(count: number): number | string {
   }
   return result;
 }
-const stroke = (str: string): string => {
-  let text: string[] = str.split(' '); // разбиваем строку на слова
 
-  let string: string = text.reduce((longest, current) => {
-    return current.length > longest.length ? current : longest; // используем метод reduce для нахождения самого длинного слова(1 значение будет самым длинным словом, 2 значение - текущее слово)
-  }, '');
-  return string;
+//2. Написать функцию, которая на вход принимает строку и возвращает длину самого длинного слова в этой строке
+const longestLine = (str: string): string => {
+  const text: string[] = str.split(' '); // разбиваем строку на слова
+  const longestItem: string = text.reduce((longest, current) => {
+    return current.length > longest.length ? current : longest; // используем метод reduce для нахождения самого длинного слова(1 значение будет самым длинным словом,
+  }, ''); // 2 значение - текущее слово)
+  return longestItem;
 };
 
-const arrayFunc = (array: number[]): number => {
-  let maxNumber: number = array.reduce((a, b) => Math.max(a, b)); // используем метод reduce для нахождения самого большого числа
+//3. Написать функцию, которая на вход принимает массив, состоящий из массивов целых чисел
+// и возвращает массив, состоящий из наибольшего числа каждого представленного подмассива.
+const arrayMaxNumber = (array: number[]): number => {
+  const maxNumber: number = array.reduce((a, b) => Math.max(a, b)); // используем метод reduce для нахождения самого большого числа
   return maxNumber;
 };
 
-const editStroke = (str: string, count: number): string => {
-  let stroke: string = str;
-  if (str.length <= count) {
-    return (stroke = str);
-  }
-  if (str.length > count) {
-    stroke = str.substring(0, count) + '...'; // срезаем строку до нужного количества символов
-  }
-
-  return stroke;
+//4. Написать функцию, которая на вход принимает строку и целое число и обрезает строку(первый аргумент),
+// если она длиннее заданной максимальной длины строки(второй аргумент) и возвращает обрезанную строку с "..." в конце.
+const editLine = (str: string, count: number): string => {
+  let text: string = '';
+  str.length > count ? (text = str.substring(0, count) + '...') : (text = str); // если длина строки больше заданной длины, то обрезаем строку
+  return text;
 };
 
+//5. Написать функцию, которая на вход принимает строку и возвращает эту строку в формате:
+//каждое слово начинается с заглавной буквы, остальные в нижнем регистре
 const strUp = (str: string): string => {
-  let text: string[] = str.split(' '); // разбиваем строку на слова
-
-  let string: string[] = text.map((item) => {
+  const text: string[] = str.split(' '); // разбиваем строку на слова
+  const editorText: string[] = text.map(item => {
     return item.charAt(0).toUpperCase() + item.slice(1); // переводим первую букву в верхний регистр
   });
-  return string.join(' ');
+  return editorText.join(' ');
 };
 
-const arrays = (array: number[], array2: number[], n: number): number[] | string => {
-  let resultArrays: number[] = [];
-  resultArrays.push(...array, ...array2.splice(0, n)); // объединяем два массива и удаляем из второго массива n элементов
+//Написать функцию, которая на вход принимает 2 массива и целое число n. Функция должна скопировать каждый элемент 1 массива
+//во 2 по порядку начиная с индекса n второго массива и вернуть полученный массив. Входные массивы не должны изменяться.
+//Например: при входных данных ([1,2,3], [4,5], 1) должно вернуться [4,1,2,3,5].
+const unionArrays = (array: number[], array2: number[], n: number): number[] | string => {
+  const resultArrays: number[] = []; // не изменяя исходный массив. slice делает именно это, возвращая новый массив
+  resultArrays.push(...array, ...array2.slice(0, n)); // объединяем два массива и удаляем из второго массива n элементов
   return resultArrays;
 };
 
-const strArray = (strArray: string[]): boolean | null | string => {
+//8. Написать функцию, которая на вход принимает массив, состоящий из двух строк
+//Функция должна вернуть true, если строка в первом элементе массива содержит
+//все буквы строки во втором элементе массива (регистр игнорируется)
+const equalityStroke = (strArray: string[]): boolean | null | string => {
   if (strArray.length > 2) {
     return 'Ошибка вы не можете создать масссив больше двух элементов';
   }
-
-  let result: boolean = strArray[0]
+  const result: boolean = strArray[0]
     .toLowerCase()
     .toUpperCase()
-    .includes(strArray[1].toLowerCase().toUpperCase()); // проверяем одинаковы ли строки
+    .includes(strArray[1].toLowerCase().toUpperCase()); // проверяем одинаковы ли строки приведенные к нижнему регистру и верхнему
   return result.toString();
 };
 
-const func9 = (array: string[], count: number): string[][] => {
+//9. Написать функцию, которая на вход принимает массив и целое число
+//Функция должна разбить массив(первый аргумент) на группы длиной size
+//(второй аргумент) и вернуть их в виде двумерного массива.
+const subArrays = (array: string[], count: number): string[][] => {
   const result: string[][] = [];
-
   for (let i = 0; i < array.length; i += count) {
     const subArray = array.slice(i, i + count); // разбиваем на подмассивы длиной count
     result.push(subArray);
@@ -71,7 +79,8 @@ const func9 = (array: string[], count: number): string[][] => {
   return result;
 };
 
-// 10 Функция рекурсия
+//10. Используя рекурсию(без использования циклов). Написать функцию,
+//которая принимает массив и целое число (n) и заполняет массив числами от n до 1
 const resultRecourse: number[] = [];
 const recourse = (array: number[], count: number) => {
   if (count <= 0) {
